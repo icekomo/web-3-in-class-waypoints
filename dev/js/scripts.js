eb307c9898cb920cf74e0d354ceeeb8d98abb381
icekomo <ios@gdovindesigns.com>
2020-02-06 09:46:34 -0500

    added scollto gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,10 +1,9 @@
 // @codekit-prepend "navigation.js";
-
 // @codekit-prepend "burger/burger.js";
 // @codekit-prepend "burger/burger-mouseenter.js";
 // @codekit-prepend "burger/burger-mouseleave.js";
 // @codekit-prepend "burger/burger-click.js";
 
 $( document ).ready(function() {
-    gsap.registerPlugin(DrawSVGPlugin);
+    gsap.registerPlugin(DrawSVGPlugin,ScrollToPlugin);
 });
