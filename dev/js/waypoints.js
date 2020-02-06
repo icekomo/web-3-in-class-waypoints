97942d59f3e33049e28120b854f0552faaa85338
icekomo <ios@gdovindesigns.com>
2020-02-06 11:38:13 -0500

    updated inview

--- dev/js/waypoints.js
+++ dev/js/waypoints.js
@@ -5,21 +5,73 @@
 //   }
 // })
 
-var inview = new Waypoint.Inview({
+var inviewBelieve = new Waypoint.Inview({
   element: $('#believe')[0],
   enter: function(direction) {
-     $.notify('Enter triggered with direction ' + direction,{ globalPosition: 'right middle', className:"success" })
+     // $.notify('Enter triggered with direction ' + direction,{ globalPosition: 'right middle', className:"success" })
+     headerBarChange(direction, scrollDirection = "enter");
   },
   entered: function(direction) {
-    $.notify('Enter triggered with direction ' + direction,{ globalPosition: 'right middle', className:"success" })
+    // $.notify('Entered triggered with direction ' + direction,{ globalPosition: 'right middle', className:"success" })
+        headerBarChange(direction , scrollDirection = "entered");
   },
   exit: function(direction) {
-    $.notify('Enter triggered with direction ' + direction,{ globalPosition: 'right middle', className:"info" })
+    // $.notify('Exit triggered with direction ' + direction,{     globalPosition: 'right middle', className:"info" })
+        headerBarChange(direction , scrollDirection = "exit");
   },
   exited: function(direction) {
-    $.notify('Enter triggered with direction ' + direction,{ globalPosition: 'right middle', className:"info" })
+    // $.notify('Exited triggered with direction ' + direction,{ globalPosition: 'right middle', className:"info" })
+    headerBarChange(direction , scrollDirection = "exited");
   },offset: {
     top: 67 // 67px
   }
-
 })
+
+
+var inviewBelieve = new Waypoint.Inview({
+  element: $('#white-background-content')[0],
+  enter: function(direction) {
+     // $.notify('Enter triggered with direction ' + direction,{ globalPosition: 'right middle', className:"success" })
+     headerBarChange(direction, scrollDirection = "enter");
+  },
+  entered: function(direction) {
+    // $.notify('Entered triggered with direction ' + direction,{ globalPosition: 'right middle', className:"success" })
+        headerBarChange(direction , scrollDirection = "entered");
+  },
+  exit: function(direction) {
+    // $.notify('Exit triggered with direction ' + direction,{     globalPosition: 'right middle', className:"info" })
+        headerBarChange(direction , scrollDirection = "exit");
+  },
+  exited: function(direction) {
+    // $.notify('Exited triggered with direction ' + direction,{ globalPosition: 'right middle', className:"info" })
+    headerBarChange(direction , scrollDirection = "exited");
+  },offset: {
+    top: 67 // 67px
+  }
+})
+
+function headerBarChange(direction, scrollDirection){
+
+    if(direction === "up" & scrollDirection === "enter"){
+        gsap.to("header",{duration:0.5,backgroundColor:"#000"});
+        // change the burger
+        gsap.to(".lines",{duration:0.25, scale:0,stagger:0.15})
+        gsap.to(".lines",{duration:0.25, scale:1, stroke:"#fff",stagger:0.15,delay:0.25})
+    }else if(direction === "up"& scrollDirection === "entered"){
+        gsap.to("header",{duration:0.25,backgroundColor:"#fff"});
+        // change the burger
+        gsap.to(".lines",{duration:0.25, scale:0,stagger:0.15})
+        gsap.to(".lines",{duration:0.25, scale:1, stroke:"#D87337",stagger:0.15,delay:0.25})
+    }else if(direction === "down"& scrollDirection === "exit"){
+        gsap.to("header",{duration:0.25,backgroundColor:"#000"});
+        // change the burger
+        gsap.to(".lines",{duration:0.25, scale:0,stagger:0.15})
+        gsap.to(".lines",{duration:0.25, scale:1, stroke:"#fff",stagger:0.15,delay:0.25})
+    } else if(direction === "down"& scrollDirection === "exited"){
+        gsap.to("header",{duration:0.25,backgroundColor:"#fff"});
+        // change the burger
+        gsap.to(".lines",{duration:0.25, scale:0,stagger:0.15})
+        gsap.to(".lines",{duration:0.25, scale:1, stroke:"#D87337",stagger:0.15,delay:0.25})
+    }
+
+}
